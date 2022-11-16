ad64c30dc3b63f0aa32d183479d28e63d83b8664
Aseel Hamayel <63041611+Aseel1818@users.noreply.github.com>
2022-11-16 21:03:37 +0200

    Add files via upload
    In this assignment, you need to follow the above steps to perform the following tasks using scala:
    
    read the json-formated tweets in the attached file and use MongoSpark library to insert them into mongoDB database in a collection called 'tweets'
    The timestamp associated with each tweet is to be stored as a Date object, where the timestamp field is to be indexed.
    Also, the geo-coordinates of tweets should be indexed properly to ensure a fast spatial-based retrieval
    calculate the number of occurrences of word w published within a circular region of raduis (r), having a central point of (lon, lat), mentioned in tweets published during the time interval (start, end). Perform this operation by two ways:
    using MongoSpark, by collecting tweets and filtering them spatio-temporally using dataframe apis.
    using mongodb library by sending a normal mongoDB query to filter by time and space.
    Text indexing is optional
    Run the application as follows:
    WordFreqCalculator.scala w r lon lat start end
    
    w: word to calculte its frequency
    
    r: raduis in meters
    
    lon: longitude
    
    lat: latitude
    
    start: starting epoc time
    
    end: ending epoc time

---
+++ MongodbTweetsWordFreqCalculator.scala
@@ -0,0 +1,117 @@
+import org.apache.spark.sql.SparkSession
+import com.mongodb.client.model.Indexes
+import com.mongodb.client.model
+import com.mongodb.MongoClient
+import com.mongodb.client.model.Filters.{and, geoWithinCenterSphere, gte, lte}
+import com.mongodb.spark.MongoSpark
+import org.apache.spark.sql.functions.{coalesce, col, to_timestamp, udf}
+import java.text.SimpleDateFormat
+import scala.collection.JavaConversions.iterableAsScalaIterable
+
+
+object MongodbTweetsWordFreqCalculator extends App {
+
+  val spark = SparkSession.builder().master("local[*]")
+    .appName("scalaExam")
+    .config("spark.mongodb.input.uri", "mongodb://localhost:27017/tweetsDB.tweets")
+    .config("spark.mongodb.output.uri", "mongodb://localhost:27017/tweetsDB.tweets").getOrCreate()
+
+  import spark.implicits._
+
+
+  /////////// Read the json data /////////////////////////////////////////////////////////////////
+
+  val tweetsJson = spark.read
+    .json("/Users/aseelhamayel/Downloads/boulder_flood_geolocated_tweets/boulder_flood_geolocated_tweets.json")
+
+  /////////// change  Timestamp to Date object  ////////////
+
+  var reformatTimestampDF = tweetsJson.withColumn(
+    "created_at",
+    coalesce(to_timestamp(col("created_at"), "E MMM dd HH:mm:ss Z yyyy")))
+
+  val timestamp = to_timestamp(reformatTimestampDF("created_at"), "MM-dd-yyyy HH:mm:ss")
+
+  reformatTimestampDF = reformatTimestampDF.withColumn("created_at", timestamp)
+
+  /////////// save the data from json to mongodb collection called tweets ///////////////////
+
+  MongoSpark.save(reformatTimestampDF)
+
+  /////// create indexes for geo-coordinates , created_at and text
+
+  val mongoClient = new MongoClient()
+  val database = mongoClient.getDatabase("tweetsDB")
+  val collection = database.getCollection("tweets")
+  //collection.createIndex(Indexes.geo2dsphere("geo.coordinates"))
+  collection.createIndex(Indexes.geo2dsphere("coordinates.coordinates"))
+  collection.createIndex(Indexes.ascending("created_at"))
+  collection.createIndex(Indexes.text("text"))
+
+
+  ////// Inputs /////////
+
+  println("please enter the word you want :")
+  val word = scala.io.StdIn.readLine()
+
+  println("please enter r :")
+  val radius = scala.io.StdIn.readInt()
+
+  println("please enter longitude  :")
+  val lon = scala.io.StdIn.readDouble()
+
+  println("please enter latitude :")
+  val lat = scala.io.StdIn.readDouble()
+
+  println("please enter  start date as format  yyyy-MM-dd : ")
+  val start = scala.io.StdIn.readLine()
+
+  println("please enter  end date as format  yyyy-MM-dd : ")
+  val end = scala.io.StdIn.readLine()
+
+  val date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+
+  val startDate = date.parse(start)
+
+  val endDate = date.parse(end)
+
+
+  //////////////// using mongodb library by sending a normal mongoDB query to filter by time and space//////////////
+
+  val spatioTemporalFilter = collection.find(and(model.Filters.regex("text", word), gte("created_at", startDate), lte("created_at", endDate),
+    geoWithinCenterSphere("coordinates.coordinates", lon, lat, radius)))
+
+  val finalSpatioTemporalFilter = spatioTemporalFilter.toArray
+
+  ////////////////////// Find the frequency of word in the filtered tweets /////////////////
+  val textOfTweets = finalSpatioTemporalFilter.map(tweet => tweet.getString("text"))
+
+  val finalCount = textOfTweets.flatMap(line => line.split(" ")).aggregate(0)(
+    (counter, element) => if (element == word) counter + 1 else counter, _ + _)
+
+  println("********************************************************")
+  println("The the number of occurrences of word you entered = " + finalCount)
+
+  /////// using MongoSpark, by collecting tweets and filtering them spatio-temporally using dataframe apis.///////////
+
+  def within(radius: Double) = udf((lat: Double, long: Double) => ???)
+
+  val mongoDBDF = MongoSpark.load(spark)
+
+  val newMongoDF = mongoDBDF.where(within(radius)($"lat", $"long")).toDF()
+
+  val newDF = newMongoDF.filter(mongoDBDF("text").contains(word))
+    .filter(mongoDBDF("created_at").geq(startDate) && mongoDBDF("created_at").leq(startDate))
+
+  val textOfTweetsDF = newDF.select("text").map(element => element.toString()).collect().toArray
+
+  val finalCountDF = textOfTweetsDF.flatMap(line => line.split(" "))
+    .aggregate(0)((counter, element) => if (element == word) counter + 1 else counter, _ + _)
+
+
+  println("********************************************************")
+  println("The the number of occurrences of word you entered = " + finalCountDF)
+
+
+}
+
